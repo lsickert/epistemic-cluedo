dd9d66781d8a98628e6c71c4c4d766917bc9ae41
Ludwig Sickert <ludwigsickert@gmail.com>
2022-06-05 21:46:47 +0200

    added some more helper functions and started working on game initialization

--- cluedo/game/helper.py
+++ cluedo/game/helper.py
@@ -1,10 +1,15 @@
 """This module contains various helper functions that are used throughout the rest of the game modules."""
 
 import random
+import json
+
+_game_resources = None
+
 
 def throw_single_dice():
     """Returns a random number from 1-6 to simulate a dice throw."""
-    return random.randint(1,6)
+    return random.randint(1, 6)
+
 
 def throw_double_dice():
     """Adds two random numbers from 1-6 together to simulate a throw with two dices."""
@@ -12,3 +17,44 @@ def throw_double_dice():
     d2 = throw_single_dice()
 
     return d1 + d2
+
+
+def _load_resources() -> None:
+    """loads the game resource specifications"""
+    global _game_resources
+    with open("../resources/resources.json", encoding="utf8") as f:
+        _game_resources = json.loads(f)
+
+
+def get_resources() -> dict:
+    """returns the full game resource specifications"""
+
+    if _game_resources is None:
+        _load_resources()
+
+    return _game_resources
+
+
+def get_characters() -> list:
+    """returns the character specifications"""
+
+    if _game_resources is None:
+        _load_resources()
+
+    return _game_resources["characters"]
+
+
+def get_weapons() -> list:
+    "returns the weapon specifications"
+    if _game_resources is None:
+        _load_resources()
+
+    return _game_resources["weapons"]
+
+
+def get_rooms() -> list:
+    """returns the room specifications"""
+    if _game_resources is None:
+        _load_resources()
+
+    return _game_resources["rooms"]
---
+++ cluedo/game/init.py
@@ -0,0 +1,60 @@
+"""this module contains functions needed to initialize a game run"""
+from typing import Tuple
+
+import random
+import helper
+import copy
+import itertools
+
+
+def create_resource_sets(num_characters: int = 6, num_weapons: int = 6, num_rooms: int = 9) -> tuple:
+    """creates the set of characters, weapons and rooms that will be used in a game"""
+    characters = _get_id_list(helper.get_characters())
+    weapons = _get_id_list(helper.get_weapons())
+    rooms = _get_id_list(helper.get_rooms())
+
+    random.shuffle(characters)
+    random.shuffle(weapons)
+    random.shuffle(rooms)
+
+    del characters[num_characters:]
+    del weapons[num_weapons:]
+    del rooms[num_rooms:]
+
+    return characters, weapons, rooms
+
+
+def create_card_deck(character_list: list, weapon_list: list, room_list: list) -> Tuple(tuple, list):
+    """
+    create the deck of playing cards.
+
+    Returns:
+    goal_deck: a tuple of one character, weapon and room to be used as the set to find in the game
+    clue_deck: a list with all remaining cards in random order
+    """
+    characters = copy.deepcopy(character_list)
+    weapons = copy.deepcopy(weapon_list)
+    rooms = copy.deepcopy(room_list)
+
+    goal_deck = characters.pop(), weapons.pop(), rooms.pop()
+    clue_deck = characters.extend(weapons.extend(rooms))
+
+    random.shuffle(clue_deck)
+
+    return goal_deck, clue_deck
+
+
+def get_card_combinations(characters: list, weapons: list, rooms: list) -> list:
+    """return the number of card combinations that are possible in a game"""
+
+    comb_list = itertools.product(characters, weapons, rooms)
+    return comb_list
+
+
+def _get_id_list(l: list) -> list:
+    """return a list with only the id values of a specification resource"""
+    id_list = []
+    for e in l:
+        id_list.append(e["id"])
+
+    return id_list

fdf669259ab40014b2a6fb3f393eab5e40f1c284
Ludwig Sickert <ludwigsickert@gmail.com>
2022-06-10 15:33:27 +0200

    fixed hand card model function

--- cluedo/game/player.py
+++ cluedo/game/player.py
@@ -5,6 +5,7 @@ import cluedo.logic_checker.kripke_model as kripke
 import random
 import itertools
 
+
 class Player:
     """all functions and properties of an individual player"""
 
@@ -24,7 +25,7 @@ class Player:
 
         random_world_id = random.randint(0, len(self.goal_model.worlds)-1)
 
-        suggestion = []  
+        suggestion = []
         for prop in self.goal_model.worlds[random_world_id].assignment:
             suggestion.append(prop)
 
@@ -33,26 +34,28 @@ class Player:
     def check_hand_cards(self, suggestion):
         """checks if the player has one of the suggested cards in his own hand cards"""
         temp_sugg = set(suggestion)
-        possible_matches = [card for card in self.hand_cards if card in temp_sugg]
+        possible_matches = [
+            card for card in self.hand_cards if card in temp_sugg]
 
         if len(possible_matches) > 0:
             random.shuffle(possible_matches)
 
             return possible_matches[0]
-        
+
         else:
             return possible_matches
-    
+
     def build_hand_cards_model(self, player_id: str, num_hand_cards: int, possible_cards: list):
 
-        combinations = itertools.product(possible_cards, repeat=num_hand_cards)
+        combinations = itertools.combinations(possible_cards, num_hand_cards)
 
-        self.hand_card_models[str(player_id)] = kripke.create_single_kripke_model(combinations)
+        self.hand_card_models[str(player_id)] = kripke.create_single_kripke_model(
+            combinations)
 
     def _update_model_with_hand_cards(self):
-        
+
         for card in self.hand_cards:
             formula = formulas.not_has_specific_card(card)
 
-            self.goal_model = kripke.update_kripke_model(self.goal_model, formula)
-
+            self.goal_model = kripke.update_kripke_model(
+                self.goal_model, formula)

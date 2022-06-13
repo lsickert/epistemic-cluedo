c5388dc3c27083be691c389b0a94ec117430019b
Ludwig Sickert <ludwigsickert@gmail.com>
2022-06-13 12:19:33 +0200

    added some more weapons and prevent choosing more than available options

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 """epistemic-cluedo main module"""
 import cluedo.game.core as game
+import cluedo.game.helper as helper
 
 if __name__ == "__main__":
-    num_players = int(input("How many players? (default 3) "))
+    max_players = len(helper.get_characters())
+    max_weapons = len(helper.get_weapons())
+    max_rooms = len(helper.get_rooms())
+
+    num_players = int(
+        input(f"How many players? (default 3, max {max_players}) "))
+
+    while num_players > max_players:
+        num_players = int(
+            input(f"Please choose at most {max_players} players! "))
+
     controllable_players = int(
         input("How many controllable players? (default 1, 0 for automatic game) "))
-    num_characters = int(input("How many characters? (default 5, max 6) "))
-    num_weapons = int(input("How many weapons? (default 5, max 6) "))
-    num_rooms = int(input("How many rooms? (default 5, max 9) "))
+
+    while controllable_players > num_players:
+        controllable_players = int(input(
+            f"You cannot choose more controllable players than the maximal number of players. Please choose a number below {num_players}! "))
+
+    num_characters = int(
+        input(f"How many characters? (default 5, max {max_players}) "))
+
+    while num_characters > max_players:
+        num_characters = int(
+            input(f"Please choose at most {max_players} characters! "))
+
+    num_weapons = int(
+        input(f"How many weapons? (default 5, max {max_weapons}) "))
+
+    while num_weapons > max_weapons:
+        num_weapons = int(
+            input(f"Please choose at most {max_weapons} weapons! "))
+
+    num_rooms = int(input(f"How many rooms? (default 5, max {max_rooms}) "))
+
+    while num_rooms > max_rooms:
+        num_rooms = int(input(f"Please choose at most {max_rooms} rooms! "))
 
     game.start_game(num_players, controllable_players,
                     num_characters, num_weapons, num_rooms)

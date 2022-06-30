bbe0cb7f9d9cf7feed8afe80675ecf43e9dafc4b
Thijs Visee <thijsvisee@gmail.com>
2022-06-30 18:54:18 +0200

    removed tqdm

--- cluedo/game/core.py
+++ cluedo/game/core.py
@@ -4,43 +4,27 @@ import cluedo.game.init as init
 import cluedo.logic_checker.kripke_model as kripke
 import cluedo.logic_checker.formulas as formulas
 import cluedo.game.player as player_class
-from tqdm import tqdm
 import random
 
 
 def start_game(num_players: int = 6, controllable_players=1, num_characters: int = 6, num_weapons: int = 6, num_rooms: int = 9):
-    pbar = tqdm(desc="Starting game setup", total=(
-        num_players*2 + num_players*num_players + 5))
-
-    pbar.set_description("Create resources")
-    pbar.update(1)
     characters, weapons, rooms = init.create_resource_sets(
         num_characters, num_weapons, num_rooms)
 
-    pbar.set_description("Create card deck")
-    pbar.update(1)
     goal_deck, clue_deck = init.create_card_deck(characters, weapons, rooms)
 
-    pbar.set_description("Calculate possible card combinations")
-    pbar.update(1)
     possible_worlds = init.get_card_combinations(characters, weapons, rooms)
 
-    pbar.set_description("Create baseline Kripke model")
-    pbar.update(1)
     base_model = kripke.create_multi_kripke_model(possible_worlds, num_players)
 
     players = {}
 
-    pbar.set_description("Split hand cards between players")
-    pbar.update(1)
     hand_cards = _split_hand_cards(num_players, clue_deck)
 
     list_of_colors = ["scarlett", "green", "mustard", "plum", "peacock", "white"]
 
     # initialize players
     for player in range(num_players):
-        pbar.set_description(f"Create player {str(player+1)}")
-        pbar.update(1)
         color = random.choice(list_of_colors)   # Assign a color to each player, important for game rules and starting positions
         list_of_colors.remove(color)            # Remove color from list, such that each player has an unique color.
         players[str(player+1)] = player_class.Player((player+1), hand_cards[player],
@@ -48,9 +32,6 @@ def start_game(num_players: int = 6, controllable_players=1, num_characters: int
 
     # build the hand card models of the other players for each player
     for player in players.values():
-        pbar.set_description(
-            f"Create hand card knowledge for player {str(player.player_id)}")
-        pbar.update(1)
 
         player.build_own_hand_cards_model(num_players)
 
@@ -62,14 +43,9 @@ def start_game(num_players: int = 6, controllable_players=1, num_characters: int
             remaining_clues.remove(card)
 
         for other_player in players.values():
-            pbar.set_description(
-                f"Create hand card knowledge for player {str(player.player_id)} about player {str(other_player.player_id)}")
-            pbar.update(1)
             if not other_player.player_id == player.player_id:
                 player.build_hand_cards_model(
                     other_player.player_id, num_hand_cards, remaining_clues)
-
-    pbar.close()
 
     print("starting game")
 
